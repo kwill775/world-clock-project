fa9ba1ae23baea46cb0f23d807b5b8226ff58615
kwill775 <spn775@yahoo.com>
2024-09-26 14:01:12 -0700

    added link to home

--- src/index.js
+++ src/index.js
@@ -42,7 +42,9 @@ function updateCity(event) {
         <div class="time">${cityTime.format(
           "h:mm:ss"
         )} <small>${cityTime.format("A")}</small></div>
-    </div>`;
+    </div>
+    <a href="/">Home</a>
+    `;
 }
 
 updateTime();
